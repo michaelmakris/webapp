55800403d94913f80e6667bb0c96e1e9a37e3785
michaelmakris <makrinari@hotmail.com>
2020-05-30 12:02:24 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# Web_app" 
+"# webapp" 
--- WebApp_DjangoAzure/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\michail markis\documents\web app (django)\webapp_djangoazure\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
